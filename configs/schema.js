08ed8c02bb7be8865d3bd3a7990a8b65b05531ed
karthik <karthiknadar1204@gmail.com>
2025-05-30 08:39:20 +0530

    db setup done

---
+++ configs/schema.js
@@ -0,0 +1,10 @@
+import { integer, pgTable, serial, text, timestamp } from 'drizzle-orm/pg-core';
+export const usersTable = pgTable('users_table', {
+  id: serial('id').primaryKey(),
+  name: text('name').notNull(),
+  email: text('email').notNull().unique(),
+  image: text('image').notNull(),
+  refreshToken: text('refresh_token').notNull(),
+  createdAt: timestamp('created_at').notNull().defaultNow(),
+  updatedAt: timestamp('updated_at').notNull().defaultNow(),
+});
